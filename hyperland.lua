fbd8ac52ea9523323e9d02f5aa8186628c068b66
perfektnacht <nlmash1263@gmail.com>
2026-06-23 22:59:16 -0400

    Add: hyprland lua files

---
+++ hyperland.lua
@@ -0,0 +1,23 @@
+require("modules.monitors")
+require("modules.programs")
+require("modules.autostart")
+require("modules.environment")
+require("modules.looknfeel")
+require("modules.misc")
+require("modules.input")
+require("modules.keybindings")
+require("modules.windows")
+
+-- folder hierarchy:
+--   hyprland.lua
+--   notes.md
+--   modules <dir>
+-- > -- > modules/monitors.conf
+--        modules/programs.conf
+--        modules/autostart.conf
+--        modules/environment.conf
+--        modules/looknfeel.conf
+--        modules/misc.conf
+--        modules/input.conf
+--        modules/keybindings.conf
+--        modules/windows.conf
